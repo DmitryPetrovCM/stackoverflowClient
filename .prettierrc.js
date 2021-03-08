c76345be77ff2794a7ee6efcdaeae151526f0566
dmitriy_petrov1 <Dmitriy_Petrov1@epam.com>
2021-03-08 13:39:33 +0400

    install and setup necessary dependencies, add git-hooks

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+};
